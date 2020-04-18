72c26de0773e4d11835e0a74a3fa24b96896ed4f
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-18 22:03:40 +0530

    basic components added

--- App.js
+++ App.js
@@ -4,16 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Meal App</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
